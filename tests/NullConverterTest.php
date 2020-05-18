231a7caf5307dbeef8c022f1020bedd93d0b2983
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2020-05-18 17:02:29 +0800

    added nullconverter test

---
+++ tests/NullConverterTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Cion\TextToSpeech\Tests;
+
+use Cion\TextToSpeech\Converters\NullConverter;
+
+class NullConverterTest extends TextToSpeechTestCase
+{
+    /** @test */
+    public function it_should_return_the_path()
+    {
+        $textToConvert = 'test';
+
+        $result = (new NullConverter)->convert($textToConvert);
+
+        $this->assertSame('TTS/' . md5($textToConvert) . '.mp3', $result);
+    }
+}
