c685c57a750063f01290bed61e9d1be5f13ab9d5
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2020-05-18 17:02:41 +0800

    added polly converter test wip

---
+++ tests/PollyConverterTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Cion\TextToSpeech\Tests;
+
+use Mockery as m;
+
+class PollyConverterTest extends TextToSpeechTestCase
+{
+    /** @test */
+    public function it_should_save_to_the_specified_disk()
+    {
+    
+    }
+}
