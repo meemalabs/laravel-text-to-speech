adbefd71fbd02ac9d06b15428addce04edf39e14
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2020-05-18 09:03:20 +0000

    Apply fixes from StyleCI

--- tests/PollyConverterTest.php
+++ tests/PollyConverterTest.php
@@ -2,13 +2,10 @@
 
 namespace Cion\TextToSpeech\Tests;
 
-use Mockery as m;
-
 class PollyConverterTest extends TextToSpeechTestCase
 {
     /** @test */
     public function it_should_save_to_the_specified_disk()
     {
-    
     }
 }
